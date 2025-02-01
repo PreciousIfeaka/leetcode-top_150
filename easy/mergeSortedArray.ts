d0d175cc924d5225ad5b23f95cd890df7a300c23
PreciousIfeaka <preciousifeaka@gmail.com>
2025-02-01 19:51:33 +0100

    merge sorted array

---
+++ easy/mergeSortedArray.ts
@@ -0,0 +1,14 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+ function merge(nums1: number[], m: number, nums2: number[], n: number): number[] {
+  for (let i = 0; i < n; i++) {
+      nums1[m + i] = nums2[i];
+  };
+  nums1.sort((a, b) => a - b);
+  return nums1;
+};
+
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
+console.log(merge([1], 1, [], 0));
+console.log(merge([0], 0, [1], 1));
